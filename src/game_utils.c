4ec105805f68a4970a651b30ca5c206bc069d1c8
Gabriel <abg.2jz1810@gmail.com>
2025-01-17 10:45:51 -0300

    seg fault when destroying images fixed

--- src/game_utils.c
+++ src/game_utils.c
@@ -1,5 +1,32 @@
 #include "../main.h"
 
+static void	destroy_images(t_game *game)
+{
+	if (game->ground)
+        mlx_destroy_image(game->mlx, game->ground);
+    if (game->astronaut)
+        mlx_destroy_image(game->mlx, game->astronaut);
+    if (game->wall)
+        mlx_destroy_image(game->mlx, game->wall);
+    if (game->up)
+        mlx_destroy_image(game->mlx, game->up);
+    if (game->left_up)
+        mlx_destroy_image(game->mlx, game->left_up);
+    if (game->left)
+        mlx_destroy_image(game->mlx, game->left);
+    if (game->left_down)
+        mlx_destroy_image(game->mlx, game->left_down);
+    if (game->down)
+        mlx_destroy_image(game->mlx, game->down);
+    if (game->right_down)
+        mlx_destroy_image(game->mlx, game->right_down);
+    if (game->right)
+        mlx_destroy_image(game->mlx, game->right);
+    if (game->right_up)
+        mlx_destroy_image(game->mlx, game->right_up);
+
+}
+
 int	close_window(int key, t_game *game)
 {
     t_matrix *map;
@@ -7,9 +34,7 @@ int	close_window(int key, t_game *game)
     map = game->map;
 	if (key == 65307)
 	{
-        mlx_destroy_image(game->mlx, game->ground);
-        mlx_destroy_image(game->win, game->wall);
-		//mlx_destroy_image(game->mlx_ptr, game->exit);
+		destroy_images(game);
 		mlx_destroy_window(game->mlx, game->win);
 		mlx_destroy_display(game->mlx);
 		free(game->mlx);
